32565cc1939b58ee5cd75b02d877815e57235599
Pardeep Dhingra <Pardeep.Dhingra@sial.com>
2016-12-26 15:23:08 +0530

    Categories api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :products
+      resources :categories, only: [:index]
       get 'search' => 'products#search'
     end
   end
